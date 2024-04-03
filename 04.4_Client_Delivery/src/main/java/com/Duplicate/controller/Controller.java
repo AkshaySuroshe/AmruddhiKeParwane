d31534dd4d0010d92df9877f34d85fe0ec2834dd
AkshaySuroshe <akshaysuroshe4@gmail.com>
2024-04-03 15:05:52 +0530

    Akshay sent u the Task

---
+++ 04.4_Client_Delivery/src/main/java/com/Duplicate/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.Duplicate.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	
+}//
---
+++ 04.4_Client_Delivery/src/test/java/com/Duplicate/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Duplicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
