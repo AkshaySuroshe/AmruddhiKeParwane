a7b8d9ed8f6708bb887e691f4a5f88684f23320c
HP <HP@DESKTOP-INOE733>
2024-04-03 15:33:59 +0530

    mrunal's code

--- 04.4_Client_Delivery/src/main/java/com/Duplicate/controller/Controller.java
+++ 04.4_Client_Delivery/src/main/java/com/Duplicate/controller/Controller.java
@@ -5,9 +5,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 	
+	
+	public void mrunal()
+	{
+		System.out.println("mrunal this side");
+	}
+	
 	public static void main(String[] args) {
+		
+		Controller c=new Controller();
+		c.mrunal();
+				
+		 
+		
 		System.out.println("Git demo");
 	}
+  
 
 	
 }
